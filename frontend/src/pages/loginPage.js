7417001a662cd8f1e9014e7076709003ae132c31
kartikaekhurana <you@example.com>
2025-05-17 22:21:53 +0530

    Update loginPage.js

--- frontend/src/pages/loginPage.js
+++ frontend/src/pages/loginPage.js
@@ -23,28 +23,25 @@ const LoginPage = () => {
       const response = await fetch('https://attendease-e5bz.onrender.com/api/login', {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json',
+          'Content-type': 'application/json',
         },
         body: JSON.stringify({ email: emailInput, password: passwordInput }),
       });
 
       if (!response.ok) {
-        const errorData = await response.json();
-        console.log('Error response:', errorData);
         throw new Error('Login failed');
       }
 
       const data = await response.json();
       dispatch(loginSuccess(data));
-      navigate('/dashboard');
     } catch (err) {
-      console.error('Error:', err.message);
+      console.error('Error:', err);
       alert('Invalid credentials');
     }
   };
 
   const handleRegister = () => {
-    navigate('/registeruser');
+    navigate('/registeruser'); // Redirect to the Register User page
   };
 
   return (
@@ -58,25 +55,34 @@ const LoginPage = () => {
         <p className='logo-text'>AttendEase - Smart Attendance</p>
       </div>
       <div className="login-container">
-        <form onSubmit={handleSubmit}>
+        <form>
           <div className="form-cluster">
             <div className='email-box'>
               <label htmlFor="emailInput">Email address</label>
               <input
                 id='emailInput'
                 type="email"
+                className="form-control"
+                aria-describedby="emailHelp"
                 placeholder="Enter email"
                 onChange={handleInputChange}
                 style={{backgroundColor: "rgba(0,0,0,0.4)", color: "white", border: "none"}}
               />
             </div>
-            <small style={{ color: 'rgba(0,0,0,0.5)' }}>We'll never share your email with anyone else.</small>
+            <small
+              id="emailHelp"
+              style={{ color: 'rgba(0,0,0,0.5' }}
+              className="form-text"
+            >
+              We'll never share your email with anyone else.
+            </small>
           </div>
           <div className="pass-group">
             <label htmlFor="passwordInput">Password</label>
             <input
               id='passwordInput'
               type="password"
+              className="form-control"
               placeholder="Password"
               onChange={handleInputChange}
               style={{backgroundColor: "rgba(0,0,0,0.4)", color: "white", border: "none"}}
@@ -84,10 +90,26 @@ const LoginPage = () => {
           </div>
           <div className='register-box'>
             New User?
-            <button type='button' onClick={handleRegister}>Register user</button>
+            <button
+              className='registerbtn'
+              type='button'
+              onClick={handleRegister}
+            >
+              Register user
+            </button>
           </div>
+
+
+
+
           <div style={{ textAlign: 'center' }}>
-            <button type="submit">Submit</button>
+            <button
+              className='submitbtn'
+              type="submit"
+              onClick={handleSubmit}
+            >
+              Submit
+            </button>
           </div>
         </form>
       </div>
